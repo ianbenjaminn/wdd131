12ca3f12ce35d118c6de19e56ac22100b5dc166f
ianbenjaminn <mil21025@byui.edu>
2025-09-20 12:47:34 -0600

    mission st8ment complete!

---
+++ ponder_wk1/meta-css.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = PI * radius * radius * PI;
+
+console.log(area);
+
+radius = 20;
+                    
+area = PI * radius * radius * PI;
+
+console.log(area);
