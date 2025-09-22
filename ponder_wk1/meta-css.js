02f13a7c706842fdf94ff19927acc0d869311174
ianbenjaminn <mil21025@byui.edu>
2025-09-22 11:53:07 -0600

    ponderwk1 complete

--- ponder_wk1/meta-css.js
+++ ponder_wk1/meta-css.js
@@ -10,3 +10,19 @@ radius = 20;
 area = PI * radius * radius * PI;
 
 console.log(area);
+
+const one = 1;
+const two = '2';
+
+let result = one * two
+console.log(result);
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "Ian";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+//console.log(student); //does not work, can't access a block variable outside the block
+                    
